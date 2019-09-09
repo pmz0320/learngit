0a8e326dfdf67c14d79036345f116d8dacd0cda6
xuwei3 <xuwei3@asiainfo-sec.com>
2019-09-09 23:28:48 +0800

    修改一个类

--- src/com/linkage/Demo.java
+++ src/com/linkage/Demo.java
@@ -2,6 +2,8 @@ package src.src.com.linkage;
 
 public class Demo {
     public static void main(String[] args) {
+
+        String name = "hello world";
         System.out.println("Hello World");
         System.out.println("Hello World 1");
     }
